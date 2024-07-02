3ab0ea2c351ca4ffa519e1558a89987a369e1f48
Jude <drury_yl@yahoo.com>
2024-07-02 11:29:16 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LatihanEFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class AgamaModel
+    {
+        [Column("fs_kd_agama")]
+        public string AgamaId { get; set; }
+        [Column("fs_nm_agama")]
+        public string AgamaName { get; set; }
+    }
+
+    public class DataSosialDbContext : DbContext
+    {
+        public DbSet<AgamaModel> Agama { get; set; }
+        
+        public DataSosialDbContext(DbContextOptions<DataSosialDbContext> options) : base(options)
+        {
+        }
+        public override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=LatihanEFCore;Trusted_Connection=True;");
+        }
+    }
+}
